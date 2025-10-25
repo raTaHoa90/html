693207189d4d640efb3642d5ba69fdba605b3f97
raTaHoa90 <160627104+raTaHoa90@users.noreply.github.com>
2025-10-25 13:14:13 +0300

    JS start

---
+++ js/study1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var asd = 123;
+let varNumberCount = 123,
+    var_number_count = 412n,
+    VarNumberCount;
+const NAME_FIELD = 'field';
+console.log(var - number - count);
+/*
+undefined
+Number
+String
+Boolean
+null
+Object
+    Object
+    Function
+    Array
+    Date
+    RegExp
+
+BigInt
+Symbol
+Map
+Set
+WeakMap
+WeakSet
+*/
